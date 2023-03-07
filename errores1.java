5daddf2897d2063416b253ab4b1a3e6da611fc30
Ignacio Agustin Aprikian <ignacio-aprikian@outlook.com>
2023-03-07 17:30:21 -0300

    Clase 2 7/3

---
+++ errores1.java
@@ -0,0 +1,8 @@
+public class errores1 {
+    public static void main(String[] args) {
+        String nombre;
+        Boolean bandera;
+        Character a;
+        }
+        }
+
---
+++ variables/Variables.java
@@ -0,0 +1,15 @@
+package variables;
+
+
+public class Variables {
+    
+public static void main(String[] args) {
+    Byte num = 5;
+    String carlos = "teemo";
+    Long num2 = (long) 99;
+    Float s3 = (float) 7;
+
+
+}
+    
+}
