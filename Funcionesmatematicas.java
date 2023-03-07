5f5a08ea0f93d0f70013ba977bb62ed27ddc1747
Ignacio Agustin Aprikian <ignacio-aprikian@outlook.com>
2023-03-07 17:59:41 -0300

    Arimetica

---
+++ Funcionesmatematicas.java
@@ -0,0 +1,13 @@
+public class Funcionesmatematicas {
+   public static void main(String[] args) {
+    
+    int num1 = 5;
+    int num2 = 5;
+    
+    int suma = num1+num2;
+
+    double division = num1/num2;
+
+    boolean logico = num2 < num1;
+   } 
+}
---
+++ variables/Arimetica.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class Arimetica {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 5;
+        int suma = num1 + num2;
+        double division = num1 / num2;
+        boolean logico = num2 < num1;
+        num1++;
+        }
+}
